d67af93b9eda95d50ae4b19fd74b86831fa4cb1a
Poli <raimundophilippe@gmail.com>
2019-03-09 18:13:18 +0100

    Update factorio_mods_update.sh

--- lgsm/functions/factorio_mods_update.sh
+++ lgsm/functions/factorio_mods_update.sh
@@ -5,25 +5,30 @@
 # Website: https://linuxgsm.com
 # Description: Handles updating mods Factorio servers.
 
-local commandname="UPDATE-MODS"
-local commandaction="Update-mods"
+local commandname="UPDATE"
+local commandaction="Update"
 local function_selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 #Variables
 
+username="IIPoliII"
+token="******"
+
 declare ver
 declare installed_mods
 
-fn_get_factorio_mods(){
+mkdir -p ${tmpdir}/factorio
+rm -rf ${tmpdir}/factorio/mods-download
+mkdir -p ${tmpdir}/factorio/mods-download
+
+fn_check_download_factorio_mods(){
         installed_mods=$(grep -Po '"name":.*?[^\\]",' ${serverfiles}/mods/mod-list.json | tr -d '"' | cut -c 7- | sed 's/.$//')
-}
-fn_download_factorio_mods(){
-        echo $installed_mods
         while read -r installed_mods; do
                 (
                 while [[ ${get_mod_version} != "null" ]] ; do
-                        ((ver++))
-                        get_mod_version=$(curl -s --request GET https://mods.factorio.com/api/mods/bobpower | jq ".releases[${ver}] .info_json .factorio_version" | sed -e 's/^"//' -e 's/"$//')
+                        ver=$((ver+1))
+                        get_mod_version=$(curl -s --request GET https://mods.factorio.com/api/mods/${installed_mods} | jq ".releases[${ver}] .info_json .factorio_version" | sed -e 's/^"//' -e 's/"$//')
+                        download_url="${installed_mods}:"
                         echo "${get_mod_version}:${ver}"
                 done
                 ) | while read ver_check
@@ -31,16 +36,37 @@ fn_download_factorio_mods(){
                                 currentbuild=$(grep "Loading mod base" "${serverfiles}/factorio-current.log" 2> /dev/null | awk '{print $5}' | tail -1 | cut -f1,2 -d'.')
                                 ver=$(cut -d ":" -f 2 <<< "$ver_check")
                                 mod_ver=$(echo "${ver_check}" | cut -f1 -d":")
+                                echo $installed_mods
                                 echo current bld : $currentbuild
                                 echo arraynb : $ver
                                 echo mod_ver : $mod_ver
-                                if [[ $mod_ver == $currentbuild ]]; then
-                                        get_mod_download=$(curl -s --request GET https://mods.factorio.com/api/mods/bobpower | jq ".releases[${ver}] .download_url" | sed -e 's/^"//' -e 's/"$//')
-                                        echo $get_mod_download
+                                if [[ ${mod_ver} == ${currentbuild} ]]; then
+                                        get_mod_download=$(curl -s --request GET https://mods.factorio.com/api/mods/${installed_mods} | jq ".releases[${ver}] .download_url" | sed -e 's/^"//' -e 's/"$//')
+                                        echo "https://mods.factorio.com${get_mod_download}" > ${tmpdir}/factorio/mods-download/$installed_mods
                                 fi
                         done
-        done <<< "$x"
+        done <<< "${installed_mods}"
 }
-fn_get_factorio_mods
+fn_download_factorio_mods(){
+        mv ${serverfiles}/mods/mod-list.json ${tmpdir}/factorio/mod-list.json
+        login_data="?username=${username}&token=${token}"
+        for mod_dl in "${tmpdir}/factorio/mods-download"/*
+        do
+                download=$(cat "$mod_dl")
+                download_full=$(echo "${download}${login_data}")
+                echo $download_full lin
+                wget ${download_full} ${serverfiles}/mods
+        done
+        for file in *.*; do
+                mv "$file" "$(basename "$file" .*).zip"
+        done
+        mv ${tmpdir}/factorio/mod-list.json ${serverfiles}/mods/mod-list.json
+}
+fn_check_download_factorio_mods
+files=(${tmpdir}/factorio/mods-download/*)
+if [ ${#files[@]} -gt 0 ]; then
 fn_download_factorio_mods
+else
+:
+fi
 
